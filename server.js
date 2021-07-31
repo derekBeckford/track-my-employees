935ffcea182a1529ba7ce1927a99bd2054619de6
Derek Beckford <dbeck1207@icloud.com>
2021-07-31 16:35:31 -0500

    logic to the rest of the choices from inquirer

--- server.js
+++ server.js
@@ -30,12 +30,12 @@ const prompt = () => {
         message: "What Would you like to do?",
         choices: [
           "View All Employees",
-          "View All Employeees by Department",
+          "View All Departments",
           "View All Roles",
+          "Add Role",
+          "Add Department",
           "Add Employee",
-          "Remove Employee",
           "Update Employee Role",
-          "Update Employees Manager",
         ],
       },
     ])
@@ -45,12 +45,20 @@ const prompt = () => {
           viewAllEmployees();
           break;
 
-        case "View All Employeees by Department":
-          employeeByDepartment();
+        case "View All Departments":
+          viewAllDepartments();
           break;
 
         case "View All Roles":
           viewAllRoles();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Add Department":
+          addDepartment();
           break;
 
         case "Add Employee":
@@ -60,17 +68,13 @@ const prompt = () => {
         case "Update Employee Role":
           updateEmployeeRole();
           break;
-
-        case "Update Employees Manager":
-          updateEmployeesManager();
-          break;
       }
     });
 };
 
 const viewAllEmployees = () => {
-  const sql = `select emp.first_name, emp.last_name, rl.title, rl.salary, dpt.department_name, CONCAT(mng.first_name, CONCAT (' ', mng.last_name)) AS manager from employee emp join employee mng join roles rl join department dpt on emp.manager_id = mng.id and emp.role_id = rl.id AND rl.department_id = dpt.id;`;
-  db.query(sql, function (err, result, fields) {
+  const sql = `select emp.id, emp.first_name, emp.last_name, emp.title, emp.salary, emp.department_name, CONCAT(mng.first_name, CONCAT (' ', mng.last_name)) AS manager from (select emp.id, emp.first_name, emp.last_name, rl.title, rl.salary, dpt.department_name, emp.manager_id from employee emp join roles rl join department dpt on emp.role_id = rl.id AND rl.department_id = dpt.id) emp left join employee mng on emp.manager_id = mng.id;`;
+  db.query(sql, function (err, result) {
     if (err) throw err;
     console.table(result);
     prompt();
@@ -79,7 +83,16 @@ const viewAllEmployees = () => {
 
 const employeeByDepartment = () => {
   const sql = `select emp.first_name, emp.last_name, dpt.department_name from employee emp join roles rl join department dpt on emp.role_id = rl.id AND rl.department_id = dpt.id;`;
-  db.query(sql, function (err, result, fields) {
+  db.query(sql, function (err, result) {
+    if (err) throw err;
+    console.table(result);
+    prompt();
+  });
+};
+
+const viewAllDepartments = () => {
+  const sql = `SELECT * FROM department;`;
+  db.query(sql, function (err, result) {
     if (err) throw err;
     console.table(result);
     prompt();
@@ -87,12 +100,109 @@ const employeeByDepartment = () => {
 };
 
 const viewAllRoles = () => {
-  const sql = `SELECT rl.title FROM roles rl;`;
+  const sql = `SELECT rl.title, rl.id, dept.department_name, rl.salary FROM roles rl JOIN department dept ON rl.department_id = dept.id;`;
   db.query(sql, function (err, result) {
     if (err) throw err;
     console.table(result);
     prompt();
   });
+};
+
+const addRole = () => {
+  getAllDepartments().then((departments) => {
+    inquirer
+      .prompt([
+        {
+          name: "title",
+          type: "input",
+          message: "Enter the title of the role: ",
+          validate: (addTitle) => {
+            if (addTitle) {
+              return true;
+            } else {
+              console.log("Please enter a title");
+              return false;
+            }
+          },
+        },
+        {
+          name: "salary",
+          type: "input",
+          message: "What is this roles salary?",
+          validate: (addSalary) => {
+            const salary = Number(addSalary);
+
+            if (!isNaN(salary)) {
+              return true;
+            } else {
+              console.log("Please enter a Salary");
+              return false;
+            }
+          },
+        },
+        {
+          name: "department",
+          type: "list",
+          message: "What is the associated department for this role?",
+          choices: departments,
+        },
+      ])
+
+      .then((val) => {
+        let deptIdPromise = db
+          .promise()
+          .query(
+            `SELECT id FROM department WHERE department_name = '${val.department}'`
+          )
+          .then(([rows, fields]) => {
+            return rows[0].id;
+          });
+        deptIdPromise
+          .then((departmentId) => {
+            sql = `INSERT INTO roles SET ?;`;
+            db.query(
+              sql,
+              {
+                title: val.title,
+                salary: val.salary,
+                department_id: departmentId,
+              },
+              function (err) {
+                if (err) throw err;
+              }
+            );
+          })
+          .then(() => {
+            prompt();
+          });
+      });
+  });
+};
+
+const addDepartment = () => {
+  inquirer
+    .prompt([
+      {
+        name: "departmentname",
+        type: "input",
+        message: "Enter the department name: ",
+        validate: (addDepartment) => {
+          if (addDepartment) {
+            return true;
+          } else {
+            console.log("Please enter a department name");
+            return false;
+          }
+        },
+      },
+    ])
+    .then((val) => {
+      sql = `INSERT INTO department SET?;`;
+      db.query(sql, { department_name: val.departmentname });
+    })
+    .then(() => {
+      prompt();
+    });
 };
 
 const getAllManagers = () => {
@@ -107,84 +217,166 @@ const getAllManagers = () => {
     });
 };
 
-function addEmployee() {
-  getAllManagers().then((managers) => {
-    inquirer
-      .prompt([
-        {
-          name: "firstname",
-          type: "input",
-          message: "Enter their first name",
-          validate: (addFirstName) => {
-            if (addFirstName) {
-              return true;
-            } else {
-              console.log("Please enter a first name");
-              return false;
-            }
-          },
-        },
-        {
-          name: "lastname",
-          type: "input",
-          message: "Enter their last name",
-          validate: (addLastName) => {
-            if (addLastName) {
-              return true;
-            } else {
-              console.log("Please enter a last name");
-              return false;
-            }
-          },
-        },
-        {
-          name: "role",
-          type: "list",
-          message: "What is their role?",
-          choices: [
-            "Salesperson",
-            "Lead Engineer",
-            "Engineer",
-            "Accountant",
-            "Lawyer",
-            "CEO",
-          ],
-        },
-        {
-          name: "manager",
-          type: "list",
-          message: "What their managers name?",
-          choices: managers,
-        },
-      ])
-      .then((val) => {
-        let roleIdPromise = db
-          .promise()
-          .query(`SELECT id FROM roles WHERE title = '${val.role}'`)
-          .then(([rows, fields]) => {
-            return rows[0].id;
-          });
-          // let managerIdPromise = db
-          // .promise()
-          // .query(`SELECT id FROM employee where `)
-        roleIdPromise.then((roleId) => {
-          const sql = `INSERT INTO employee SET ?`;
-          db.query(
-            sql,
-            {
-              first_name: val.firstname,
-              last_name: val.lastname,
-              role_id: roleId,
-              //manager_id: val.manager,
-            },
-            function (err) {
-              if (err) throw err;
-              //console.table(val);
+const getAllRoles = () => {
+  const sql = `SELECT title FROM roles;`;
+  return db
+    .promise()
+    .query(sql)
+    .then(([rows, fields]) => {
+      let roles = [];
+      for (var i in rows) roles.push(rows[i].title);
+      return roles;
+    });
+};
 
-              //prompt();
-            }
-          );
+const getAllDepartments = () => {
+  const sql = `SELECT department_name FROM department;`;
+  return db
+    .promise()
+    .query(sql)
+    .then(([rows, fields]) => {
+      let departments = [];
+      for (var i in rows) departments.push(rows[i].department_name);
+      return departments;
+    });
+};
+
+const getRoleId = (role) => {
+  return db
+    .promise()
+    .query(`SELECT id FROM roles WHERE title = '${role}'`)
+    .then(([rows, fields]) => {
+      return rows[0].id;
+    });
+};
+
+const getManagerId = (manager) => {
+  return db
+    .promise()
+    .query(
+      `SELECT id FROM employee WHERE CONCAT(first_name, CONCAT(' ', last_name)) = '${manager}'`
+    )
+    .then(([rows, fields]) => {
+      return rows[0].id;
+    });
+};
+
+function addEmployee() {
+  getAllRoles().then((roles) => {
+    getAllManagers().then((managers) => {
+      inquirer
+        .prompt([
+          {
+            name: "firstname",
+            type: "input",
+            message: "Enter their first name",
+            validate: (addFirstName) => {
+              if (addFirstName) {
+                return true;
+              } else {
+                console.log("Please enter a first name");
+                return false;
+              }
+            },
+          },
+          {
+            name: "lastname",
+            type: "input",
+            message: "Enter their last name",
+            validate: (addLastName) => {
+              if (addLastName) {
+                return true;
+              } else {
+                console.log("Please enter a last name");
+                return false;
+              }
+            },
+          },
+          {
+            name: "role",
+            type: "list",
+            message: "What is their role?",
+            choices: roles,
+          },
+          {
+            name: "manager",
+            type: "list",
+            message: "What their managers name?",
+            choices: managers,
+          },
+        ])
+        .then((val) => {
+          let roleIdPromise = getRoleId(val.role);
+          let managerIdPromise = getManagerId(val.manager);
+          roleIdPromise.then((roleId) => {
+            managerIdPromise.then((managerId) => {
+              const sql = `INSERT INTO employee SET ?`;
+              db.query(
+                sql,
+                {
+                  first_name: val.firstname,
+                  last_name: val.lastname,
+                  role_id: roleId,
+                  manager_id: managerId,
+                },
+                function (err) {
+                  if (err) throw err;
+                }
+              );
+            });
+          });
+        })
+        .then(() => {
+          prompt();
         });
-      });
+    });
   });
 }
+
+getAllEmpoyees = () => {
+  const sql = `SELECT CONCAT(first_name, CONCAT(' ', last_name)) as full_name FROM Employee order by first_name, last_name asc`;
+  return db
+    .promise()
+    .query(sql)
+    .then(([rows, fields]) => {
+      let employees = [];
+      for (var i in rows) employees.push(rows[i].full_name);
+      return employees;
+    });
+};
+
+const removeEmployee = () => {
+  getAllEmpoyees().then((employees) => {});
+};
+
+const updateEmployeeRole = () => {
+  getAllEmpoyees().then((employees) => {
+    getAllRoles().then((roles) => {
+      inquirer
+        .prompt([
+          {
+            name: "employeename",
+            type: "list",
+            message: "Select employee to update role:",
+            choices: employees,
+          },
+          {
+            name: "role",
+            type: "list",
+            message: "Select a new role: ",
+            choices: roles,
+          },
+        ])
+        .then((val) => {
+          let roleIdPromise = getRoleId(val.role);
+          roleIdPromise.then((roleId) => {
+            sql = `UPDATE employee SET role_id = '${roleId}' WHERE CONCAT(first_name, CONCAT(' ', last_name)) = '${val.employeename}';`;
+            db.query(sql);
+          });
+        })
+        .then(() => {
+          prompt();
+        });
+    });
+  });
+};
